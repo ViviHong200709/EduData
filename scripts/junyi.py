d40244c2d778012699f10c3e93836b47c07c070e
Vivian Hong <1044652353@qq.com>
2021-09-01 19:32:31 +0800

    add note

--- scripts/junyi.py
+++ scripts/junyi.py
@@ -1,11 +1,13 @@
-# coding: utf-8
-# 2021/09/01 @ hongyuting
-
-from longling import path_append
-from EduData.DataSet.junyi import extract_relations, build_json_sequence
-
-
-def test_junyi_kt(shared_data_dir):
-    src_root = path_append(shared_data_dir, "junyi", to_str=True)
-    ku_dict_path = path_append(shared_data_dir, "junyi", "data", "graph_vertex.json")
-    build_json_sequence(src_root, path_append(src_root, "data", to_str=True), ku_dict_path,n=950,max_record_len=10)
+# # coding: utf-8
+# # 2021/09/01 @ hongyuting
+#
+# from longling import path_append
+# from EduData.DataSet.junyi import extract_relations, build_json_sequence
+#
+#
+# def test_junyi_kt(shared_data_dir):
+#     src_root = path_append(shared_data_dir, "junyi", to_str=True)
+#     ku_dict_path = path_append(shared_data_dir, "junyi", "data", "graph_vertex.json")
+#     build_json_sequence(src_root, path_append(src_root, "data", to_str=True), ku_dict_path,n=950,max_record_len=10)
+#
+# test_junyi_kt('../data')
