93bc07df57bb4918275583775e25454eb5db206c
Vivian Hong <1044652353@qq.com>
2021-09-01 18:49:31 +0800

    remove test

--- scripts/junyi.py
+++ scripts/junyi.py
@@ -9,4 +9,3 @@ def test_junyi_kt(shared_data_dir):
     src_root = path_append(shared_data_dir, "junyi", to_str=True)
     ku_dict_path = path_append(shared_data_dir, "junyi", "data", "graph_vertex.json")
     build_json_sequence(src_root, path_append(src_root, "data", to_str=True), ku_dict_path,n=950,max_record_len=10)
-test_junyi_kt('../data/')
